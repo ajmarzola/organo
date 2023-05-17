edcb19416de5f276e5d3f0cb2cacab6cda1a28a1
Anderson José Marzola <ajmarzola@gmail.com>
2023-05-17 19:42:59 -0300

    Olá, Mundo e Primeiro Componente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* Traduzindo */}
+        <p>
+          Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+        <p>
+          Bem-vindo ao React =D.
         </p>
         <a
           className="App-link"
