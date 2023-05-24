16e6cb8ceb3328ec4156dca0f102af167ed5a07e
Anderson José Marzola <ajmarzola@gmail.com>
2023-05-24 17:31:39 -0300

    Manipulação do input nome

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 export const CampoTexto = (props) => {
-    //console.log(props);
+    console.log(props);
     const placeholderModificado = `${props.placeholder}...`;
+    
+    const [valor, setValor] = useState('Valor Padrão'); 
+
+    const aoDigitar = (evento) => {
+        setValor(evento.target.value);
+        console.log(valor);
+    }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={placeholderModificado} required={props.obrigatorio}></input>
+            <input placeholder={placeholderModificado} required={props.obrigatorio} onChange={aoDigitar} value={valor} />
         </div>
     );
 }
