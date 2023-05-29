e1b0c4c12c5d9d18aa4f7735b1fab1f9eaf84138
Anderson José Marzola <ajmarzola@gmail.com>
2023-05-29 16:45:00 -0300

    Estilos dinamicos nos times

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,9 +1,12 @@
 import './Time.css';
 
 export const Time = (props) => {
+    const styleSection = { backgroundColor: props.corSecundaria };
+    const styleH3 = { borderColor: props.corPrimaria };
+
     return (
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={styleSection}>
+            <h3 style={styleH3}>{props.nome}</h3>
         </section>
     );
 }
