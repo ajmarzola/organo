050305ce0bfe03c380cab21ea26661561f13e58e
Anderson José Marzola <ajmarzola@gmail.com>
2023-05-29 15:54:25 -0300

    Inclusão dos Times

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoCadastrar={colaborador => aoAdicionar(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
     </div>
   );
 }
