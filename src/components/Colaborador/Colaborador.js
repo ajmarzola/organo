8dd41f60e52757756fae8cb47bd9cb27ddd8c4e3
Anderson José Marzola <ajmarzola@gmail.com>
2023-06-05 19:03:39 -0300

    sincronizando com a ultima versão da aula

--- src/components/Colaborador/Colaborador.js
+++ src/components/Colaborador/Colaborador.js
@@ -1,9 +1,11 @@
 import './Colaborador.css';
 
 export const Colaborador = ({ nome, cargo, imagem, corDeFundo }) => {
+    var divStyle = { backgroundColor: corDeFundo };
+
     return (
         <div className='colaborador'>
-            <div className='cabecalho' style={ backgroundColor = corDeFundo }>
+            <div className='cabecalho' style={divStyle}>
                 <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
